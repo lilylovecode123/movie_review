c0c4993254569d16caecdb99c7e78115772ad2c6
Die Zhou <2748030Z@student.gla.ac.uk>
2023-03-19 16:54:18 +0000

    CheckPassword Test

--- movie_review_app/tests.py
+++ movie_review_app/tests.py
@@ -1,10 +1,14 @@
-from django.test import TestCase
+from multiprocessing.connection import Client
 
 # Create your tests here.
 from django.test import TestCase
 from django.urls import reverse
 from django.db import models
 
+from movie_review.movie_review_app.models import User
+
+
+# Define a test case for the login function
 class LoginTestCase(TestCase):
 
     def setUp(self):
@@ -45,3 +49,38 @@ class LoginTestCase(TestCase):
         # Check that the response contains an error message
         self.assertIn('User inputs a wrong password.', response.json()['error'])
 
+class CheckPasswordViewTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_correct_password(self):
+        # Set up a user with a known password
+        username = 'testuser'
+        password = 'testpassword'
+        user = User.objects.create_user(username=username, password=password)
+
+        # Log in as the user
+        self.client.login(username=username, password=password)
+
+        # Call the checkPassword view with the correct original password
+        response = self.client.get('/check_password/', {'originPwd': password, 'token': user.id})
+
+        # Check that the response is a success response
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {'status': 'success'})
+
+    def test_incorrect_password(self):
+        # Set up a user with a known password
+        username = 'testuser'
+        password = 'testpassword'
+        user = User.objects.create_user(username=username, password=password)
+
+        # Log in as the user
+        self.client.login(username=username, password=password)
+
+        # Call the checkPassword view with an incorrect original password
+        response = self.client.get('/check_password/', {'originPwd': 'wrongpassword', 'token': user.id})
+
+        # Check that the response is a warning response
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {'status': 'warning', 'message': 'User inputs wrong original password.'})
